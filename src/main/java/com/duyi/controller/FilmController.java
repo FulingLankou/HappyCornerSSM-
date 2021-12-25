80b67573c12f394ae83af34d94f898b4cc2e5021
HappyCorner <2876399483@qq.com>
2021-12-25 13:45:50 +0800

    第二次提交：完成home页面展示

--- src/main/java/com/duyi/controller/FilmController.java
+++ src/main/java/com/duyi/controller/FilmController.java
@@ -3,10 +3,13 @@ package com.duyi.controller;
 import com.duyi.service.FilmService;
 import com.duyi.vo.FilmVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -15,9 +18,18 @@ public class FilmController {
     @Autowired
     private FilmService filmService;
 
-    @RequestMapping("/getList")
+    @RequestMapping("/filmList")
     @ResponseBody
     public List<FilmVo> getList(){
         return filmService.getList();
     }
+
+    @RequestMapping("/home")
+    public String home(HttpServletRequest request, Model model){
+        System.out.println("request.contextPath:"+request.getContextPath());
+        List<FilmVo> filmVoList = filmService.getList();
+        model.addAttribute("filmVoList",filmVoList);
+        return "home";
+    }
+
 }
